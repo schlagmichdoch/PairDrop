24fdff8a8a3bc9c69eedcb79a73648310c37dde8
schlagmichdoch <schlagmichdoch@users.noreply.github.com>
2023-01-17 11:27:07 +0100

    adding a second stun server provided by the openrelayproject to the RTCPeer.config

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -825,9 +825,12 @@ RTCPeer.config = {
             urls: 'stun:stun.l.google.com:19302'
         },
         {
-            urls: "turn:openrelay.metered.ca:443",
-            username: "openrelayproject",
-            credential: "openrelayproject",
+            urls: 'stun:openrelay.metered.ca:80'
+        },
+        {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
         },
     ]
 }
