11a988e5509912c0c868786c09efe00f8121c04f
schlagmichdoch <schlagmichdoch@users.noreply.github.com>
2023-03-06 16:05:58 +0100

    increase version to v1.4.4

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const cacheVersion = 'v1.4.3';
+const cacheVersion = 'v1.4.4';
 const cacheTitle = `pairdrop-cache-${cacheVersion}`;
 const urlsToCache = [
     'index.html',
--- public_included_ws_fallback/service-worker.js
+++ public_included_ws_fallback/service-worker.js
@@ -1,4 +1,4 @@
-const cacheVersion = 'v1.4.3';
+const cacheVersion = 'v1.4.4';
 const cacheTitle = `pairdrop-included-ws-fallback-cache-${cacheVersion}`;
 const urlsToCache = [
     'index.html',
