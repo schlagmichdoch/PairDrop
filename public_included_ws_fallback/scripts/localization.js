19f56a84999597dcb594ea636c76d160dc039611
schlagmichdoch <schlagmichdoch@users.noreply.github.com>
2023-08-29 02:33:54 +0200

    enable Norwegian, Russian, and Chinese

--- public_included_ws_fallback/scripts/localization.js
+++ public_included_ws_fallback/scripts/localization.js
@@ -1,7 +1,7 @@
 class Localization {
     constructor() {
         Localization.defaultLocale = "en";
-        Localization.supportedLocales = ["en"];
+        Localization.supportedLocales = ["en", "nb", "ru", "zh-CN"];
         Localization.translations = {};
         Localization.defaultTranslations = {};
 
