659b53b5faf6c99e076992cc61a6890c56a60f54
schlagmichdoch <schlagmichdoch@users.noreply.github.com>
2025-02-11 14:52:44 +0100

    Increase version to v1.10.11
    ## Enhancements
    - Prevent referrer in created links in message
    
    ## Fixes
    - Prevent wrong `start_url` when installing PairDrop PWA from sub path (#329)
    - Hotfix: Disable thumbnail creation for HEIC/HEIF files until PR #350 is merged (hotfix for #336)
    
    ## Languages
    - New Language Bulgarian
    - New Language Estonian
    - New Language Basque
    - New Language Persian
    - New Language Norwegian Nynorsk
    - New Language Slovak
    - New Language Tamil
    - New Language Hant Script
    - New Language Korean
    - Translations updates from Hosted Weblate (Indonesian, Arabic, Italian, Czech, Japanese, Ukrainian, Russian, German, Portuguese (Brazil))

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const cacheVersion = 'v1.10.10';
+const cacheVersion = 'v1.10.11';
 const cacheTitle = `pairdrop-cache-${cacheVersion}`;
 const forceFetch = false; // FOR DEVELOPMENT: Set to true to always update assets instead of using cached versions
 const relativePathsToCache = [
